77a56813e1a3d055f3fd6cf9fa299e46580d2228
Laefar <53569867+Laefar@users.noreply.github.com>
2024-05-31 18:02:39 +0200

    ADD | Input field to generate new QR Code

--- src/generateQR.js
+++ src/generateQR.js
@@ -1,6 +1,21 @@
+// Create and setup QRCode Object.
 var qrcode = new QRCode(document.getElementById("qrcode"));
 
 qrcode.width = 128;
 qrcode.height = 128;
 
-currentTab = chrome.tabs.query({active: true, lastFocusedWindow: true}, url=>{qrcode.makeCode(url[0].url);});
+// Get current Active Tabs Promise.
+let currentTab = chrome.tabs.query({active: true, lastFocusedWindow: true});
+
+// Set the default value of the input field as the active tab url.
+let linkArea = document.getElementById("linkArea");
+currentTab.then(tab=>{linkArea.defaultValue = tab[0].url;});
+
+// Hook the generate QR code function to the button's on click event.
+let generateBtn = document.getElementById("generateBtn");
+generateBtn.onclick = function() {
+    qrcode.makeCode(linkArea.value);
+};
+
+// Generate the QR Code for the active tab.
+currentTab.then(tab=>{qrcode.makeCode(tab[0].url);});
